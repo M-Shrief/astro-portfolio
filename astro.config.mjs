9990182685950a331391eb75015a161bbc4844dd
M-Shrief <m.shrief.dev@proton.me>
2023-09-14 18:20:46 +0300

    simple style for BlogPost, configured Shiki theme, and removed generator meta tag.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark-dimmed',
+      wrap: true, // Enable word wrap to prevent horizontal scrolling
+    },
+  },
   integrations: []
 });
