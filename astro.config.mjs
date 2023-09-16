2e792cacf923431b99fe0acde0aaeca5407656b8
M-Shrief <m.shrief.dev@proton.me>
2023-09-16 17:32:59 +0300

    integrated mdx for more customized blogs for more customization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'github-dark-dimmed',
-      wrap: true, // Enable word wrap to prevent horizontal scrolling
-    },
+      wrap: true // Enable word wrap to prevent horizontal scrolling
+    }
   },
-  integrations: []
+
+  integrations: [mdx()]
 });
