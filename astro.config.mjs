c248d65f472b33f7f939a2732afbdd074389fcbe
M-Shrief <m.shrief.dev@proton.me>
2023-09-27 15:41:57 +0300

    added prefetch for Navbar, updated Readme.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
+import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -11,13 +11,18 @@ export default defineConfig({
       wrap: true // Enable word wrap to prevent horizontal scrolling
     }
   },
+
   site: 'https://m-shriet.tech',
   integrations: [
     mdx(),
     sitemap({
       changefreq: 'weekly',
       priority: 0.7,
-      entryLimit: 100,
+      entryLimit: 100
+    }),
+    prefetch({
+      selector: ["a[href^='/projects']", "a[href^='/blog']", "a[href^='/journey']"],
+      throttle: 1,
     })
   ]
 });
